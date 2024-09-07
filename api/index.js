f510cd29ceb52ba7db552a907e588cfbb8e9d1a2
justin假斯叮 <jusitnjuju0804@gmail.com>
2024-09-07 14:14:04 +0800

    12

---
+++ api/index.js
@@ -0,0 +1,45 @@
+// api/index.js
+
+const fs = require('fs');
+const path = require('path');
+
+module.exports = (req, res) => {
+    const filePath = path.join(__dirname, '../count.txt');
+    
+    if (req.method === 'POST') {
+        const { username } = req.body;
+        
+        fs.readFile(filePath, 'utf8', (err, data) => {
+            if (err) {
+                console.error(err);
+                res.status(500).send('服务器错误');
+                return;
+            }
+
+            let counts = JSON.parse(data);
+            counts.loginCount += 1;
+
+            fs.writeFile(filePath, JSON.stringify(counts), (err) => {
+                if (err) {
+                    console.error(err);
+                    res.status(500).send('服务器错误');
+                    return;
+                }
+                res.json(counts);
+            });
+        });
+    } else if (req.method === 'GET') {
+        fs.readFile(filePath, 'utf8', (err, data) => {
+            if (err) {
+                console.error(err);
+                res.status(500).send('服务器错误');
+                return;
+            }
+
+            const counts = JSON.parse(data);
+            res.json(counts);
+        });
+    } else {
+        res.status(405).send('方法不被允许');
+    }
+};
